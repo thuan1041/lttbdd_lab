2bd09975e712b82d4b3809d553ac7a13399c4744
thuan1041 <tranminhthuan7230@gmail.com>
2023-10-08 11:12:02 +0700

    react lab4 tiki

--- lttbdd_lab4/components/Lab4_2c.js
+++ lttbdd_lab4/components/Lab4_2c.js
@@ -1,35 +1,77 @@
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image, CheckBox} from "react-native";
 
+
 function Lab4_2c() {
+    const [password, setPassword] = useState("");
+    const [pwLength, setPwLength] = useState(8);
+    const [includeLowercase, setIncludeLowercase] = useState(true);
+    const [includeUppercase, setIncludeUppercase] = useState(true);
+    const [includeNumbers, setIncludeNumbers] = useState(true);
+    const [includeSpecialSymbols, setIncludeSpecialSymbols] = useState(true);
+
+    const generatePassword = () => {
+        let characters = "";
+        if(includeLowercase){
+            characters += "abcdefghijklmnopqrstuvwxyz";
+        }
+        if(includeUppercase){
+            characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        if(includeNumbers){
+            characters += "0123456789";
+        }
+        if(includeSpecialSymbols){
+            characters += "!@#$%^&*()_+-={}[];':,./<>?";
+        }
+        let generatedPassword = "";
+        for (let i = 0; i < pwLength; i++) {
+            const index = Math.floor(Math.random() * characters.length);
+            generatedPassword += characters[index];
+        }
+        setPassword(generatedPassword);
+    }
     return (  
         <View style={styles.container}>
             <View style={styles.bgWrapper}>
                 <View>
                     <Text style={styles.textHeader}>PASSWORD<br/>GENERATOR</Text>
                 </View>
-                <TextInput style={styles.passwordTxt}></TextInput>
+                <TextInput style={styles.passwordTxt} value={password}></TextInput>
                 <View style={styles.statusWrapper}>
                     <Text style={styles.statusTxt}>Password length</Text>
-                    <TextInput style={styles.pwLengthTxt} ></TextInput>
+                    <TextInput style={styles.pwLengthTxt}
+                        value={pwLength.toString()}
+                        onChangeText={(text) => setPwLength(parseInt(text) || 0)}>
+                    </TextInput>
                 </View>
                 <View style={styles.statusWrapper}>
                     <Text style={styles.statusTxt}>Include lower case letters</Text>
-                    <CheckBox style={styles.checkboxTxt}></CheckBox>
+                    <CheckBox style={styles.checkboxTxt} value={includeLowercase}
+                        onValueChange={(newValue) => setIncludeLowercase(newValue)}>
+                    </CheckBox>
                 </View>
                 <View style={styles.statusWrapper}>
                     <Text style={styles.statusTxt}>Include upcase letters</Text>
-                    <CheckBox style={styles.checkboxTxt}></CheckBox>
+                    <CheckBox style={styles.checkboxTxt} value={includeUppercase}
+                        onValueChange={(newValue) => setIncludeUppercase(newValue)}>
+                    </CheckBox>
                 </View>
                 <View style={styles.statusWrapper}>
                     <Text style={styles.statusTxt}>Include number</Text>
-                    <CheckBox style={styles.checkboxTxt}></CheckBox>
+                    <CheckBox style={styles.checkboxTxt} value={includeNumbers}
+                        onValueChange={(newValue) => setIncludeNumbers(newValue)}>
+                    </CheckBox>
                 </View>
                 <View style={styles.statusWrapper}>
                     <Text style={styles.statusTxt}>Include special symbol</Text>
-                    <CheckBox style={styles.checkboxTxt}></CheckBox>
+                    <CheckBox style={styles.checkboxTxt}
+                        value={includeSpecialSymbols}
+                        onValueChange={(newValue) => setIncludeSpecialSymbols(newValue)}>
+                    </CheckBox>
                 </View>
                 <View style={styles.btnWrapper}>
-                    <TouchableOpacity style={styles.btnGenerate}>
+                    <TouchableOpacity style={styles.btnGenerate} onPress={generatePassword}>
                         <Text style={styles.btnGenerateTxt}>GENERATE PASSWORD</Text>
                     </TouchableOpacity>
                 </View>
@@ -75,7 +117,8 @@ const styles = StyleSheet.create({
         height:55,
         backgroundColor:"#151537",
         marginTop:30,
-        marginBottom:30
+        marginBottom:30,
+        color:"#ffffff"
     },
     statusWrapper:{
         display:"flex",
--- lttbdd_lab4/components/Tiki_Ok.js
+++ lttbdd_lab4/components/Tiki_Ok.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from "react-native";
 
 function Tiki_Ok() {
+    const [quantity, setQuantity] = useState(1);
+    const productPrice = 141800;
+    const [totalPrice, setTotalPrice] = useState(productPrice);
+  
+    const increaseQuantity = () => {
+      setQuantity(quantity + 1);
+      setTotalPrice(productPrice * (quantity + 1));
+    };
+  
+    const decreaseQuantity = () => {
+      if (quantity > 1) {
+        setQuantity(quantity - 1);
+        setTotalPrice(productPrice * (quantity - 1));
+      }
+    };
+    const handlePlaceOrder = () => {
+        alert("Đặt hàng thành công");
+      };
     return (  
         <View style = {styles.container}>
             <View style={styles.headerWrapper}>
@@ -13,13 +32,13 @@ function Tiki_Ok() {
                         <Text style={styles.priceSaleTxt}>141.800 đ</Text>
                         
                         <View style={styles.quantityWrapper}>
-                            <TouchableOpacity style={styles.btnQuantityWrapper}>
+                            <TouchableOpacity style={styles.btnQuantityWrapper} onPress={decreaseQuantity}>
                                 <Text style={styles.btnQuantityTxt}>-</Text>
                             </TouchableOpacity>
                             <TouchableOpacity style={styles.btnQuantityTxt}>
-                                <Text style={styles.quantityTxt} >1</Text>
+                                <Text style={styles.quantityTxt} >{quantity}</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity style={styles.btnQuantityWrapper}>
+                            <TouchableOpacity style={styles.btnQuantityWrapper} onPress={increaseQuantity}>
                                 <Text style={styles.btnQuantityTxt}>+</Text>
                             </TouchableOpacity>
                             <TouchableOpacity style={styles.btnQuantityMuaSau}>
@@ -31,11 +50,39 @@ function Tiki_Ok() {
             </View>
             <View style={styles.discountSaveWrapper}>
                 <Text style={styles.discountSaveTxt}>Mã giảm giá đã lưu</Text>
-                <Text style={styles.discountSaveTxt}>Xem tại đây</Text>
+                <Text style={[styles.discountSaveTxt,{marginRight:146},{color:"#134FEC"}]}>Xem tại đây</Text>
             </View>
+            <View style={styles.discountWrapper}>
+                <TouchableOpacity style={styles.discountInputWrapper}>
+                    <TextInput style={styles.discountInput}></TextInput>
+                    <Text style={styles.discountTxt}>Mã giảm giá</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.discountBtnWrapper}>
+                    <Text style={styles.discountBtnTxt}>Áp dụng</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.bgWrapper} ></View>
             <View style={styles.bodyWrapper}>
-
+                <View style={styles.couponWrapper}>
+                    <Text style={styles.coupontxt}>Bạn có phiếu quà tặng Tiki/Got it/ Urbox?</Text>
+                    <Text style={[styles.coupontxt,{color:"#134FEC"}]}>Nhập tại đây?</Text>
+                </View>
+                <View style={[styles.bgWrapper,{height:100}]} ></View>
             </View>
+            <View style={styles.tempTotalWrapper}>
+                <Text style={styles.tempTotalTxt}>Tạm tính</Text>
+                <Text style={[styles.tempTotalTxt,{color:"#EE0D0D"}]}>{totalPrice} đ</Text>
+            </View>
+            <View style={[styles.bgWrapper,{height:100}]} ></View>
+            <View style={styles.totalWrapper}>
+                <Text style={styles.totalTxt}>Thành tiền</Text>
+                <Text style={[styles.totalTxt,{color:"#EE0D0D"}]}>{totalPrice} đ</Text>
+            </View>
+            <View style={styles.footerWrapper}>
+                    <TouchableOpacity style={styles.totalBtnWrapper} onPress={handlePlaceOrder}>
+                        <Text style={styles.totalBtnTxt} >Tiến hành đặt hàng</Text>
+                    </TouchableOpacity>
+                </View>
         </View>
     );
 }
@@ -47,31 +94,32 @@ const styles = StyleSheet.create({
         flex:1,
         display:"flex",
         flexDirection:"column",
-        alignItems:"center",
+        justifyContent:"center",
         width:"100%",
+        // backgroundColor:"#C4C4C4",
     },
     headerWrapper:{
-        flex:1,
+        // flex:1,
         display:"flex",
         width:"100%",
-        height:283,
-        // backgroundColor:"red",
+        // height:283,
+        height:400,
         flexDirection:"row",
+        backgroundColor:"#ffffff",
         
     },
     productWrapper:{
         display:"flex",
         flexDirection:"row",
         width:"100%",
-        height:"60%",
+        height:"147px",
         flexDirection:"row",
         justifyContent:"space-between",
     },
     bodyWrapper:{
-        flex:1,
+        // flex:1,
         width:"100%",
         height:283,
-        backgroundColor:"yellow",
     },
     imgTxt:{
         width:104,
@@ -82,6 +130,7 @@ const styles = StyleSheet.create({
     productDetailWrapper:{
         display:"flex",
         justifyContent:"space-evenly",
+        marginTop:16,
         marginRight:16,
     },
     productTxt:{
@@ -153,6 +202,7 @@ const styles = StyleSheet.create({
         display:"flex",
         flexDirection:"row",
         justifyContent:"space-between",
+        marginTop:-240
     },
     discountSaveTxt:{
         fontFamily:"Roboto",
@@ -160,5 +210,152 @@ const styles = StyleSheet.create({
         fontSize:14,
         lineHeight:16.41,
         textAlign:"left",
-    }
+        marginLeft:16,
+    },
+    discountWrapper:{
+        width:"100%",
+        display:"flex",
+        flexDirection:"row",
+        marginTop:30,
+        marginBottom:30,
+    },
+    discountInputWrapper:{
+        width:240,
+        height:45,
+        display:"flex",
+        flexDirection:"row",
+        alignItems:"center",
+        justifyContent:"flex-start",
+        borderColor:"#808080",
+        borderRadius:4,
+        borderStyle:"solid",
+        borderWidth:1,
+        marginLeft:16,
+    },
+    discountInput:{
+        width:32,
+        height:16,
+        backgroundColor:"#F2DD1B",
+        marginLeft:16,
+    },
+    discountTxt:{
+        fontFamily:"Roboto",
+        fontWeight:700,
+        fontSize:18,
+        lineHeight:21.09,
+        textAlign:"left",
+        marginLeft:16,
+
+    },
+    discountBtnWrapper:{
+        width:99,
+        height:45,
+        backgroundColor:"#0A5EB7",
+        display:"flex",
+        justifyContent:"center",
+        marginLeft:16,
+        borderRadius:4,
+
+    },
+    discountBtnTxt:{
+        fontFamily:"Roboto",
+        fontWeight:700,
+        fontSize:20,
+        lineHeight:23.44,
+        textAlign:"center",
+        color:"#FFFFFF",
+    },
+    couponWrapper:{
+        display:"flex",
+        flexDirection:"row",
+        width:360,
+        height:51,
+        marginTop:16,
+    },
+    coupontxt:{
+        fontFamily:"Roboto",
+        fontWeight:700,
+        fontSize:12,
+        lineHeight:14.06,
+        textAlign:"left",
+        marginLeft:16,
+    },
+    bgWrapper:{
+        width:"100%",
+        height:16,
+        backgroundColor:"#C4C4C4",
+    },
+    tempTotalWrapper:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"space-between",
+        marginTop:16,
+        marginBottom:16,
+        backgroundColor:"#ffffff",
+        height:51,
+        alignItems:"center",
+        marginBottom:0,
+        marginTop:-81,
+    },
+    tempTotalTxt:{
+        fontFamily:"Roboto",
+        fontWeight:700,
+        fontSize:18,
+        lineHeight:21.09,
+        textAlign:"left",
+        marginLeft:16,
+        marginRight:16,
+    },
+    bodyWrapper:{
+        // flex:1,
+        width:"100%",
+        height:165,
+        backgroundColor:"#ffffff",
+
+    },
+    totalWrapper:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"space-between",
+        marginTop:16,
+        backgroundColor:"#ffffff",
+        height:51,
+        alignItems:"center",
+        width:"100%",
+
+    },
+    totalTxt:{
+        fontFamily:"Roboto",
+        fontWeight:700,
+        fontSize:18,
+        lineHeight:21.09,
+        textAlign:"left",
+        marginLeft:16,
+        marginRight:16,
+    },
+    footerWrapper:{
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems:"center",
+        marginRight:16,
+        marginLeft:16,
+    },
+    totalBtnWrapper:{
+        width:"100%",
+        height:51,
+        backgroundColor:"#E53935",
+        display:"flex",
+        justifyContent:"center",
+        borderRadius:4,
+    },
+    totalBtnTxt:{
+        fontFamily:"Roboto",
+        fontWeight:700,
+        fontSize:20,
+        lineHeight:23.44,
+        textAlign:"center",
+        color:"#FFFFFF",
+    },
+    
 })

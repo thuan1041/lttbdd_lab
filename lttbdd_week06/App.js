91f00bd15f8dac34c7d8170032827956a3b1d6b8
thuan1041 <tranminhthuan7230@gmail.com>
2023-10-16 11:20:54 +0700

    week06

---
+++ lttbdd_week06/App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Screen_4a from './components/Screen_4a';
+
+export default function App() {
+  return (
+    <Screen_4a/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
